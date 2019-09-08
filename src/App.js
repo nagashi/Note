ddc393586bab23f805a7ee1353079441aba52e42
Charles ORiley <ceoriley@gmail.com>
2019-09-07 23:38:28 -0500

    Added helper functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends React.Component {
   constructor () {
@@ -19,12 +21,18 @@ class App extends React.Component {
     });
   } 
 
+  getNotes = () => {
+    axios.get(urlFor('notes'))
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render () {
     const { showNote } = this.state;
     return (
       <div className = "App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List />}
+        { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
       </div>
     );
   }
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+  return 'https://note-api-charles-oriley.herokuapp.com/' + endpoint;
+};
+
+export default urlFor;
