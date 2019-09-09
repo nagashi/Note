338522a349962d76effd4d4f56887d37f4824788
Charles ORiley <ceoriley@gmail.com>
2019-09-08 19:08:44 -0500

    New subcomponents of List

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import NoteCard from './NoteCard';
 
 class List extends React.Component {
 
   UNSAFE_componentWillMount() {
     this.props.getNotes();
-}
+  }
 
-  render () {
+  render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+
     return (
       <div className="list-container">
-        List Component
+        {cards}
       </div>
     );
   }
 }
-
 
 export default List;
