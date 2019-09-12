8acb0a64a4e85124741393108c84694580522e3d
Charles ORiley <ceoriley@gmail.com>
2019-09-12 00:47:45 -0500

    Provided error notification

---
+++ src/components/Flash.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+class Flash extends React.Component {
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.props.resetError();
+      }, 3000);
+    }
+
+    render () {
+      const { error } = this.props;
+
+      return (
+        <div className="flash-container">
+          {error}
+        </div>
+      );
+    }
+}
+
+
+export default Flash;
+ 
